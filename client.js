48f9b41ca7e4b5aa707c31f3283f0193769e4ebe
Rojal Maharjan <rojma436@student.liu.se>
2023-02-07 00:22:43 +0100

    css edit

--- client.js
+++ client.js
@@ -4,13 +4,45 @@ displayView = function() {
     // the code required to display a view
     const view = document.getElementById("view");
     let readToken = localStorage.getItem("token");
+    const body = document.querySelector("body");
     if (readToken != null) {
         const profileView = document.getElementById("profileView");
         view.innerHTML = profileView.innerText;
+        body.classList.add("bodyProfile")
+        body.classList.remove("bodyWelcome")
+        openTab("Home");
     } else {
         const welcomeView = document.getElementById("welcomeView");
         view.innerHTML = welcomeView.innerText;
+        body.classList.add("bodyWelcome")
+        body.classList.remove("bodyProfile")
     }
+};
+
+const tab = document.getElementsByClassName("tab");
+let tabOpen = "";
+function openTab(tabName) {
+    for (let i = 0; i < tab.length; i++) {
+        const block = document.getElementById(tab[i].innerText)
+        if ( tab[i].innerText == tabName ) {
+            tab[i].classList.add("active");
+            block.style.display = 'block';
+            if (tabName == 'Home') {
+                //userInfo();
+                //console.log(block.childNodes.item(1).childNodes.item(1));
+                userInfo(block.childNodes.item(1).childNodes.item(1))
+            } else if (tabName == 'Browse') {
+
+            } else if (tabName == 'Account') {
+                
+            }
+        } else{ 
+            tab[i].classList.remove("active");
+            block.style.display = 'none';
+        }
+    }
+    /* console.log(tabName);
+    console.log(tab[i].innerText); */
 };
 
 window.onload = function() {
@@ -20,11 +52,9 @@ window.onload = function() {
     displayView();
 };
 
-function login(formData){
-    console.log(formData.loginEmail.value);
-    console.log(formData.loginPassword.value);
-    let email = formData.loginEmail.value;
-    let password = formData.loginPassword.value;
+function signIn(formData){
+    let email = formData.signInEmail.value;
+    let password = formData.signInPassword.value;
     const usermessages = document.getElementById("userMessages");
     usermessages.style.display = "block";
     if (String(password).length >= 6) {
@@ -43,6 +73,60 @@ function login(formData){
         usermessages.innerText = "Passwords must be 6 characters or longer.";
     }
 };
+function passwordChange(formData){
+    let oldPassword = formData.currPsw.value;
+    let newPassword = formData.newPsw.value;
+    let rePassword = formData.repassword.value;
+    let readToken = localStorage.getItem("token");
+    const usermessages = document.getElementById("userMessages");
+    usermessages.style.display = "block";
+    if ( readToken != null) {
+        if (String(oldPassword).length >= 6 && String(newPassword).length >= 6 && String(rePassword).length >= 6) {
+            if (newPassword === rePassword) {
+                const res = serverstub.changePassword(readToken, oldPassword, newPassword);
+                if (res.success) {
+                    usermessages.style.color = "green";
+                    usermessages.innerText = res.message; 
+                    formData.currPsw.value = null;
+                    formData.newPsw.value = null;
+                    formData.repassword.value = null;
+                } else {
+                    usermessages.style.color = "red";
+                    usermessages.innerText = res.message; 
+                }
+            } else {
+                usermessages.style.color = "red";
+                usermessages.innerText = "Passwords do not match.";
+            }
+        } else {
+            usermessages.style.color = "red";
+            usermessages.innerText = "Passwords must be 6 characters or longer.";
+        }
+    } else {
+        usermessages.style.color = "red";
+        usermessages.innerText = "User Not Signed In.";
+    }
+}
+function signOut(){
+    let readToken = localStorage.getItem("token");
+    const usermessages = document.getElementById("userMessages");
+    usermessages.style.display = "block";
+    if ( readToken != null) {
+        const res = serverstub.signOut(readToken);
+        if (res.success) {
+            localStorage.removeItem("token");
+            usermessages.style.color = "green";
+            usermessages.innerText = res.message;
+            displayView();
+        } else {
+            usermessages.style.color = "red";
+            usermessages.innerText = res.message; 
+        }
+    } else {
+        usermessages.style.color = "red";
+        usermessages.innerText = "User Not Signed In.";
+    }
+}
 
 function signup(formData){
     let password = formData.signupPassword.value;
@@ -60,12 +144,12 @@ function signup(formData){
                 city: formData.city.value,
                 country: formData.country.value
             };
-            console.log(user);
+            //console.log(user);
             const res = serverstub.signUp(user);
             if (res.success) {
                 usermessages.style.color = "green";
                 usermessages.innerText = res.message;  
-                usermessages.innerText += " Login to continue.";   
+                usermessages.innerText += " Signin to continue.";   
             } else {
                 usermessages.style.color = "red";
                 usermessages.innerText = res.message; 
@@ -78,12 +162,39 @@ function signup(formData){
         usermessages.style.color = "red";
         usermessages.innerText = "Passwords do not match.";
     }
-    if(error) {
-        if (e.preventDefault) {
-            e.preventDefault();
-        } else {
-            e.returnValue = false;
-        }
-        return false;
-    }
 };
+
+function userInfo(element) {
+    //const element = document.getElementById("userInfo");
+    //console.log(element);
+    let readToken = localStorage.getItem("token");
+    if ( readToken != null) {
+        const res = serverstub.getUserDataByToken(readToken);
+        if (res.success) {
+            //console.log(res.data);
+            for (const child of element.children) {
+                if (child.id == 'name') {
+                    child.innerText = res.data.firstname+" "+res.data.familyname;
+                } else if (child.id == 'email') {
+                    child.innerText = res.data.email;
+                } else if (child.id == 'gender') {
+                    child.innerText = res.data.gender;
+                } else if (child.id == 'address') {
+                    child.innerText = res.data.city+", "+res.data.country;
+                }
+              }
+            /*element.innerHTML = "<H2 itemid='name'>"+res.data.firstname+" "+res.data.familyname+"</H2>\
+                                <br>\
+                                <h4>Email</h4>\
+                                <p itemid='email'>"+res.data.email+"</p>\
+                                <h4>Gender</h4>\
+                                <p id='gender'>"+res.data.gender+"</p>\
+                                <h4>Address</h4>\
+                                <p id='adress'>"+res.data.city+", "+res.data.country+"</p>";*/
+        } else {
+            console.log(res.message); 
+        }
+    }  else {
+        console.log("User Not Signed In.");
+    }
+}
--- temp/profile.js
+++ temp/profile.js
@@ -6,6 +6,15 @@ function openTab(tabName) {
         if ( tab[i].innerText == tabName ) {
             tab[i].classList.add("active");
             block.style.display = 'block';
+            if (tabName == 'Home') {
+                //userInfo();
+                //console.log(block.childNodes.item(1).childNodes.item(1));
+                userInfo(block.childNodes.item(1).childNodes.item(1))
+            } else if (tabName == 'Browse') {
+
+            } else if (tabName == 'Account') {
+                
+            }
         } else{ 
             tab[i].classList.remove("active");
             block.style.display = 'none';
@@ -21,3 +30,38 @@ window.onload = function() {
     // window.allert() is not allowed to be used in your implementation.
     openTab("Home")
 };
+
+function userInfo(element) {
+    //const element = document.getElementById("userInfo");
+    //console.log(element);
+    let readToken = localStorage.getItem("token");
+    if ( readToken != null) {
+        const res = serverstub.getUserDataByToken(readToken);
+        if (res.success) {
+            //console.log(res.data);
+            for (const child of element.children) {
+                if (child.id == 'name') {
+                    child.innerText = res.data.firstname+" "+res.data.familyname;
+                } else if (child.id == 'email') {
+                    child.innerText = res.data.email;
+                } else if (child.id == 'gender') {
+                    child.innerText = res.data.gender;
+                } else if (child.id == 'address') {
+                    child.innerText = res.data.city+", "+res.data.country;
+                }
+              }
+            /*element.innerHTML = "<H2 itemid='name'>"+res.data.firstname+" "+res.data.familyname+"</H2>\
+                                <br>\
+                                <h4>Email</h4>\
+                                <p itemid='email'>"+res.data.email+"</p>\
+                                <h4>Gender</h4>\
+                                <p id='gender'>"+res.data.gender+"</p>\
+                                <h4>Address</h4>\
+                                <p id='adress'>"+res.data.city+", "+res.data.country+"</p>";*/
+        } else {
+            console.log(res.message); 
+        }
+    }  else {
+        console.log("User Not Signed In.");
+    }
+}
